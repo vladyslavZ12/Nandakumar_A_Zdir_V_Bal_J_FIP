5ee2c4eed957fed7e6b22d22aa6928d3aa54544e
ashwin <ashu61998@gmail.com>
2021-02-25 07:04:50 +0530

    files created, html updated

---
+++ public/index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<header>
+<section class='logo'>
+<a href="#"><img src="./images/logo" alt="logo"></a>
+</section>
+
+<div class='nav'>
+<a href="#">Home</a>
+<a href="membership.php">Membership</a>
+<a href="partners.php">Partners</a>
+<a href="programs.php">Programs</a>
+<a href="contact.php">Contact</a>
+<a href="about.php">About Us</a>
+</div>
+<section class='mainTitle'>
+<h1>London Refrees Group</h1>
+</section>
+</header>    
+<body>
+    <section class="mainImage">
+    <img src="./images/" alt="Hockey">
+    </section>
+
+    <section class="subCategory">
+        <section class="Set1">
+        <img src="./images/" alt="">
+        <p>Lorem ipsum dolor sit amet, 
+        consectetur adipisicing elit. Offici</p>
+        </section>
+        
+        <section class="Set1">
+        <img src="./images/" alt="">
+        <p>Lorem ipsum dolor sit amet, 
+        consectetur adipisicing elit. Offici</p>
+        </section>
+        
+        <section class="Set1">
+        <img src="./images/" alt="">
+        <p>Lorem ipsum dolor sit amet, 
+        consectetur adipisicing elit. Offici</p>
+        </section>
+    </section>
+
+    <section class="detailsSection">
+        <section class="detail1">
+             <section class="detailText">
+                <h2>Role of refree</h2>
+                <p>again lorem ipsum here</p>
+
+             </section>
+             <section class="detailsImages">
+                <img src="./images/" alt="">
+             </section>
+        </section>
+
+        <section class="detail2">
+             <section class="detailText">
+                <h2>The training involved</h2>
+                <p>again lorem ipsum here</p>
+
+             </section>
+             <section class="detailsImages">
+                <img src="./images/" alt="">
+             </section>
+        </section>
+    </section>
+</body>
+
+<footer>
+<section class="footerNav">
+    
+     <section class="Copyright"> something</section>
+     <section class="socialMedia">
+    <a href=""><img src="./images/facebook" alt=""></a>
+    <a href=""><img src="./images/twitter" alt=""></a>
+    <a href=""><img src="./images/youtube" alt=""></a>
+    <a href=""><img src="./images/instagram" alt=""></a>
+    
+     </section>
+
+</section>
+</footer>
+</html>
